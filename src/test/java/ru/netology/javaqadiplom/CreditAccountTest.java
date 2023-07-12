55d34998a7b037e58c2b1d46f5bf780e3ebf1476
unknown <you@example.com>
2023-07-12 01:30:51 -0700

    readmeCredit

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,43 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+    @Test
+    public void shouldTestPayTrue() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        boolean actual = account.pay(50);
+
+        Assertions.assertTrue(actual);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+    @Test
+    public void shouldTestPayZero() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        boolean actual = account.pay(0);
+
+        Assertions.assertFalse(actual);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+    @Test
+    public void shouldTestPayNegative() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        boolean actual = account.pay(-50);
+
+        Assertions.assertFalse(actual);
+        Assertions.assertEquals(50, account.getBalance());
+    }
 }
