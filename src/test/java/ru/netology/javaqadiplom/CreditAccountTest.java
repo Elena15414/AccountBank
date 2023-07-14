3668cac97eacb7670ac0b9eb4c271d39f08cd44f
unknown <you@example.com>
2023-07-14 01:05:16 -0700

    ReadmeFin

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -56,4 +56,86 @@ public class CreditAccountTest {
         Assertions.assertFalse(actual);
         Assertions.assertEquals(50, account.getBalance());
     }
+    @Test
+    public void shouldTestPayBalanceNegative() {
+        CreditAccount account = new CreditAccount(
+                -5,
+                5_000,
+                15
+        );
+
+        boolean actual = account.pay(-50);
+
+        Assertions.assertFalse(actual);
+        Assertions.assertEquals(-5, account.getBalance());
+    }
+    @Test
+    public void shouldTestAddTrue() {
+        CreditAccount account = new CreditAccount(
+                5,
+                5_000,
+                15
+        );
+
+        boolean actual = account.add(50);
+
+        Assertions.assertTrue(actual);
+        Assertions.assertEquals(55, account.getBalance());
+    }
+    @Test
+    public void shouldTestAddFalseZero() {
+        CreditAccount account = new CreditAccount(
+                5,
+                5_000,
+                15
+        );
+
+        boolean actual = account.add(0);
+
+        Assertions.assertFalse(actual);
+        Assertions.assertEquals(5, account.getBalance());
+    }
+    @Test
+    public void shouldTestAddFalseNegative() {
+        CreditAccount account = new CreditAccount(
+                5,
+                5_000,
+                15
+        );
+
+        boolean actual = account.add(-5);
+
+        Assertions.assertFalse(actual);
+        Assertions.assertEquals(5, account.getBalance());
+    }
+    @Test
+    public void shouldTestYearChange() {
+        CreditAccount account = new CreditAccount(
+                -200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(-30, account.yearChange());
+    }
+    @Test
+    public void shouldTestYearChange2() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+    @Test
+    public void shouldTestGetCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(5000, account.getCreditLimit());
+    }
 }
