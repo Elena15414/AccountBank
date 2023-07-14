9938b8d71b1613856403704e2f97f9729bce9a62
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-07-14 18:53:56 +0400

    IllegalArgumentException class add commit

---
+++ src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
@@ -0,0 +1,8 @@
+package ru.netology.javaqadiplom;
+
+public class IllegalArgumentException extends RuntimeException{
+    public IllegalArgumentException(String msq){
+        super(msq);
+    }
+
+}
